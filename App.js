142919774be15270777247f161c9b2e1260e1b19
Nguyen Hai Dang <dangnh@paxcreation.com>
2018-11-22 16:16:44 +0700

    KDJFKJDF

--- App.js
+++ App.js
@@ -6,8 +6,9 @@
  * @flow
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import React, { Component } from 'react';
+import { Platform, StyleSheet, Text, View, Alert } from 'react-native';
+import { GoogleSignin, GoogleSigninButton, statusCodes } from 'react-native-google-signin';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -16,14 +17,40 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
+GoogleSignin.configure({
+  // scopes: ['email', 'profiles'], // what API you want to access on behalf of the user, default is email and profile
+  webClientId: '504158490440-rkgbul7isd52epclosuonr5lanan261k.apps.googleusercontent.com', // client ID of type WEB for your server (needed to verify user ID and offline access)
+});
+
 type Props = {};
 export default class App extends Component<Props> {
+
+  state = {
+    isSigninInProgress: false
+  }
+
+  _signIn = async () => {
+    try {
+      await GoogleSignin.hasPlayServices();
+      const userInfo = await GoogleSignin.signIn();
+      this.setState({ userInfo });
+    } catch (error) {
+      console.error(error);
+    }
+  }
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>Welcome to React Native!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
+
+        <GoogleSigninButton
+          style={{ width: 48, height: 48 }}
+          size={GoogleSigninButton.Size.Icon}
+          color={GoogleSigninButton.Color.Dark}
+          onPress={this._signIn}
+          disabled={this.state.isSigninInProgress} />
       </View>
     );
   }
